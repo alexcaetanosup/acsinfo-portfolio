257d36b99f2effdd99416766ca285571a06be89b
Alex Caetano <alexcaetanosup>
2026-03-25 16:08:10 -0300

    build: otimização do vite e limpeza de avisos do esbuild 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
+import tailwindcss from '@tailwindcss/vite'; // 1. Importe o plugin oficial
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'; // Importe o path
+import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(), // 2. Adicione o Tailwind ANTES do React
+    react(),       // O plugin react-swc no Vite 8 já lida melhor com oxc internamente
+  ],
   resolve: {
     alias: {
-      // Define que "@" aponta para a pasta "src"
       '@': path.resolve(__dirname, './src'),
     },
   },
+  // 3. Opcional: Garante que o CSS seja processado corretamente no build
+  build: {
+    cssMinify: 'lightningcss',
+  }
 })
 
 
